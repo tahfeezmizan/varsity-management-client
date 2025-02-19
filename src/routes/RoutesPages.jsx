4317a905e004d23ba03d9beb533973461a7f049a
tahfeezmizan <mizanahmed5316@gmail.com>
2025-02-19 19:27:06 +0600

    Clean up import statements in RoutesPages.jsx

--- src/routes/RoutesPages.jsx
+++ src/routes/RoutesPages.jsx
@@ -1,6 +1,4 @@
-import {
-    createBrowserRouter,
-} from "react-router-dom";
+import { createBrowserRouter } from "react-router-dom";
 import App from "../App";
 import Login from "../pages/Login";
 import Register from "../pages/Register";
@@ -36,8 +34,6 @@ const routes = createBrowserRouter([
         path: "/register",
         element: <Register />,
     },
-
-
 ]);
 
 export default routes;
