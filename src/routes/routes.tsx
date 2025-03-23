a76896ad2a416d3732de597ee87e91f03732c743
tahfeezmizan <mizanahmed5316@gmail.com>
2025-03-23 23:02:04 +0600

    Refactor routing structure by creating admin.routes.tsx and consolidating admin paths

--- src/routes/routes.tsx
+++ src/routes/routes.tsx
@@ -1,46 +1,32 @@
 import { createBrowserRouter } from "react-router-dom";
 import App from "../App";
-import About from "../pages/About";
-import AdminDashboard from "../pages/admin/AdminDashboard";
 import Login from "../pages/Login";
 import Register from "../pages/Register";
-import CreateAdmin from "../pages/admin/CreateAdmin";
-import CreateFaculty from "../pages/admin/CreateFaculty";
-import CreateStudent from "../pages/admin/CreateStudent";
+import { adminPaths } from "./admin.routes";
 
 const router = createBrowserRouter([
   {
     path: "/",
     element: <App />,
-    children: [
-      {
-        path: "/about",
-        element: <About />,
-      },
-    ],
   },
   {
     path: "/admin",
     element: <App />,
-    children: [
-      {
-        index: true,
-        element: <AdminDashboard />,
-      },
-      {
-        path: "create-admin",
-        element: <CreateAdmin />,
-      },
-      {
-        path: "create-faculty",
-        element: <CreateFaculty />,
-      },
-      {
-        path: "create-student",
-        element: <CreateStudent />,
-      },
-    ],
+    children: adminPaths,
   },
+  {
+    path: "/admin",
+    element: <App />,
+    children: adminPaths,
+  },
+  {
+    path: "/admin",
+    element: <App />,
+    children: adminPaths,
+  },
+
+
+
 
   {
     path: "/login",
